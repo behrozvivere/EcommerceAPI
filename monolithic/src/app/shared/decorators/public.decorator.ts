2bb1441317e20c232262615e94babe0599dfce89
Behroz <167723682+behrozvivere@users.noreply.github.com>
2025-04-25 18:51:50 +0000

    Fix import error by adding AllowAnonymous decorator

--- monolithic/src/app/shared/decorators/public.decorator.ts
+++ monolithic/src/app/shared/decorators/public.decorator.ts
@@ -2,3 +2,4 @@ import { SetMetadata } from '@nestjs/common';
 
 export const IS_PUBLIC_KEY = 'isPublic';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+export const AllowAnonymous = Public; // این خط اضافه شد برای سازگاری با کدهای قبلی
